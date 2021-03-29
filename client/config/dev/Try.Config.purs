747a7de8803f5a4c80e5e9f2b3e9c0f461002c8c
Miles Frain <miles.frain@colorado.edu>
2021-03-29 16:10:42 -0700

    point to compile server

--- client/config/dev/Try.Config.purs
+++ client/config/dev/Try.Config.purs
@@ -4,7 +4,8 @@ loaderUrl :: String
 loaderUrl = "js/output"
 
 compileUrl :: String
-compileUrl = "http://localhost:8081"
+--compileUrl = "http://localhost:8081"
+compileUrl = "https://compile.purescript.org"
 
 mainGist :: String
 mainGist = "7ad2b2eef11ac7dcfd14aa1585dd8f69"
